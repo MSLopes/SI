0fa7eac2690766c4048a190f80ceac1557cb04be
MSLopes <marco.as.lopes@gmail.com>
2011-10-09 00:07:01 +0100

    FileCypher initial load

---
+++ FileCypher/src/filecypher/FileCypher.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filecypher;
+
+/**
+ *
+ * @author MLopes
+ */
+public class FileCypher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
