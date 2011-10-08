21fe5bf751ca43f371a83984e3881a28ca6ed855
MSLopes <marco.as.lopes@gmail.com>
2011-10-08 17:36:08 +0100

    First Load of HashGenerator

---
+++ hashGenerator/src/hashgenerator/HashGenerator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashgenerator;
+
+/**
+ *
+ * @author MLopes
+ */
+public class HashGenerator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
