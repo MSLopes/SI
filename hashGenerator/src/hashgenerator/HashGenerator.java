4aa1c8e31f05260fc6e51b6032bcdd347ea28f30
MSLopes <marco.as.lopes@gmail.com>
2011-10-22 14:41:27 +0100

    Added exception to control the number of parameters

--- hashGenerator/src/hashgenerator/HashGenerator.java
+++ hashGenerator/src/hashgenerator/HashGenerator.java
@@ -14,6 +14,7 @@ public class HashGenerator {
      * @param Hash function, file to generate hash
      */
     public static void main(String[] args) throws IOException{ 
+        if(args.length <2) throw new IllegalArgumentException("Arguments missing");
         IHashGen hg = new HashGen();
         hg.Init(args[0], args[1], 0);
         hg.Generate();
